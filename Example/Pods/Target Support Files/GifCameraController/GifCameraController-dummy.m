2945a7a14284f98153da3191e5fc10e019ec8817
Lawrence Tran <lawrence@tran.com>
2016-04-07 13:28:40 -0500

    pod install

---
+++ Example/Pods/Target Support Files/GifCameraController/GifCameraController-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_GifCameraController : NSObject
+@end
+@implementation PodsDummy_GifCameraController
+@end
---
+++ Example/Pods/Target Support Files/Pods-GifCameraController_Example/Pods-GifCameraController_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GifCameraController_Example : NSObject
+@end
+@implementation PodsDummy_Pods_GifCameraController_Example
+@end
