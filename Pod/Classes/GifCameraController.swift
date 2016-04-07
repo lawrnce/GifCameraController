44276a35dc87735316a459be231e0cad5a7373c5
Lawrence Tran <lawrence@tran.com>
2016-04-07 13:30:36 -0500

    Added GifCameraController file.

---
+++ Pod/Classes/GifCameraController.swift
@@ -0,0 +1,17 @@
+//
+//  GifCameraController.swift
+//  Pods
+//
+//  Created by lola on 4/7/16.
+//
+//
+
+import UIKit
+import AVFoundation
+import ImageIO
+import MobileCoreServices
+import CoreFoundation
+
+public class GifCameraController: NSObject {
+
+}
