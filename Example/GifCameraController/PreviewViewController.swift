0b72e45e5b394d76b18d8962c9a636e64b4aa8fc
Lawrence Tran <lawrence@tran.com>
2016-04-08 22:24:04 -0500

    Removed a useless property in gif creation code.

--- Example/GifCameraController/PreviewViewController.swift
+++ Example/GifCameraController/PreviewViewController.swift
@@ -55,15 +55,8 @@ class PreviewViewController: UIViewController {
         let temporaryFile = (NSTemporaryDirectory() as NSString).stringByAppendingPathComponent("temp")
         let fileOutputURL = NSURL(fileURLWithPath: temporaryFile)
         let destination = CGImageDestinationCreateWithURL(fileOutputURL, kUTTypeGIF, self.bitmaps.count, nil)
-        let fileProperties = [kCGImagePropertyGIFDictionary as String:
-            [
-                kCGImagePropertyGIFLoopCount as String: 0
-            ],
-            kCGImageDestinationLossyCompressionQuality as String: 1.0]
-        let frameProperties = [kCGImagePropertyGIFDictionary as String:
-            [
-                kCGImagePropertyGIFDelayTime as String: self.duration / Double(self.bitmaps.count)
-            ]]
+        let fileProperties = [kCGImagePropertyGIFDictionary as String:[kCGImagePropertyGIFLoopCount as String: 0]]
+        let frameProperties = [kCGImagePropertyGIFDictionary as String:[kCGImagePropertyGIFDelayTime as String: self.duration / Double(self.bitmaps.count)]]
         CGImageDestinationSetProperties(destination!, fileProperties as CFDictionaryRef)
         
         for bitmap in self.bitmaps! {
